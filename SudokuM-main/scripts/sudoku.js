3b39c5ebb2a2834f54002dbe40cafef58b1bab04
TNL110 <phuclocloi123@gmail.com>
2023-06-03 15:02:49 +0700

    test

---
+++ SudokuM-main/scripts/sudoku.js
@@ -0,0 +1,108 @@
+class People{
+    constructor(name){
+        this.name = name;
+        this.life = 3;
+    }
+}
+
+class Sudoku {
+    constructor(container, mode = null, people, settings) {
+        this.mode = mode;
+        this.container = container;
+        let peoplearr = [];
+        for(let i = 0; i < people.length; i++){
+            peoplearr.push(new People(people[i]))
+        }
+        this.people = peoplearr;
+        if (typeof container === 'string')
+            this.container = document.querySelector(container);
+
+        this.game = new Game(util.extend(defaultConfig, settings), this.people);
+
+        this.container.appendChild(this.getGameBoard());
+    }
+    getGameBoard() {
+        return this.game.buildGUI();
+    }
+    start() {
+        this.matrix = this.game.matrix.row;
+
+        let arr = [],
+            x = 0,
+            values,
+            rows = this.game.matrix.row,
+            inputs = this.game.table.getElementsByTagName('input'),
+            difficulties = {
+                'easy': 40,
+                'normal': 30,
+                'hard': 20,
+                'impossible': 10,
+            };
+
+        // Solve the game to get the solution
+        this.game.solveGame(0, 0);
+
+        util.each(rows, function(i, row) {
+            util.each(row, function(r, val) {
+                arr.push({
+                    index: x,
+                    value: val
+                });
+                x++;
+            });
+        });
+
+        // Get random values for the start of the game
+        values = getUnique(arr, difficulties[this.game.config.difficulty() || this.mode]);
+        this.values = values;
+        this.game.valuesMatrix = this.game.matrix.row;
+
+        // Reset the game
+        this.reset();
+
+        util.each(values, function(i, data) {
+            let input = inputs[data.index];
+            input.value = data.value;
+            input.classList.add('disabled');
+            input.tabIndex = -1;
+            triggerEvent(input, 'keyup');
+        });
+    }
+    joinGame(matrix, valuesMatrix) {
+        let arr = [],
+            x = 0,
+            values,
+            rows = matrix,
+            inputs = this.game.table.getElementsByTagName('input');
+
+        // Solve the game to get the solution
+        this.game.solveGame(0, 0);
+
+        util.each(rows, function(i, row) {
+            util.each(row, function(r, val) {
+                arr.push({
+                    index: x,
+                    value: val
+                });
+                x++;
+            });
+        });
+
+        values = valuesMatrix;
+        this.game.valuesMatrix = matrix;
+
+        // Reset the game
+        this.reset();
+
+        util.each(values, function(i, data) {
+            let input = inputs[data.index];
+            input.value = data.value;
+            input.classList.add('disabled');
+            input.tabIndex = -1;
+            triggerEvent(input, 'keyup');
+        });
+    }
+    reset() {
+        this.game.resetGame();
+    }
+}
